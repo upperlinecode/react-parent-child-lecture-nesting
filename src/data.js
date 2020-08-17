3824ec21f3ba8ef213a835856a33749307437b30
jolson615 <jolson615@gmail.com>
2020-08-17 09:39:10 -0400

    adds sample js data

---
+++ src/data.js
@@ -0,0 +1,26 @@
+// You can use this example data if you're following the lecture guide. 
+let data = {
+  "artist": "My favorite singer",
+  "album": "Their best album",
+  "image": "albumCover.png",
+  "tracks": [
+    {
+      "id": 1,
+      "title": "Opening Song",
+      "duration": "2:31",
+      "comment": "makes me want to dance"
+    },
+    {
+      "id": 2,
+      "title": "Second Song",
+      "duration": "3:12",
+      "comment": "always skip this one"
+    },
+    {
+      "id": 3,
+      "title": "Third Song",
+      "duration": "5:33",
+      "comment": "my favorite!"
+    }
+  ]
+}
